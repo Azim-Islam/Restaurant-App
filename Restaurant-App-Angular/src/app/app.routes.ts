38895a81bae8c3079d92fbfd7a39bd75d91f649c
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-12-01 23:08:41 +0600

    Update app.routes.ts

--- Restaurant-App-Angular/src/app/app.routes.ts
+++ Restaurant-App-Angular/src/app/app.routes.ts
@@ -1,9 +1,35 @@
-import { Routes } from '@angular/router';
+import {Router, Routes} from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {inject} from '@angular/core';
+import {AuthService} from './login/auth.service';
+
+function canVisitDashboard(){
+  const authService = inject(AuthService);
+  const routerService = inject(Router);
+  console.log(authService.loginStatus());
+  if (authService.loginStatus() === 'valid') {
+    return true;
+  }
+  if (authService.loginStatus() === 'invalid') {
+    routerService.navigateByUrl("");
+    return false;
+  }
+  else{
+    routerService.navigateByUrl("");
+    return false;
+  }
+}
 
 export const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [canVisitDashboard],
+
   }
 ];
