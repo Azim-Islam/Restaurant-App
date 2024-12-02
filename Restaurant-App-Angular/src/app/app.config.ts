bf3e315071de493fcbaab7c56286051faac225c7
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-12-02 20:42:59 +0600

    Update app.config.ts

--- Restaurant-App-Angular/src/app/app.config.ts
+++ Restaurant-App-Angular/src/app/app.config.ts
@@ -8,14 +8,55 @@ import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {provideNzIcons} from 'ng-zorro-antd/icon';
 
 registerLocaleData(en);
+
+import {IconDefinition} from '@ant-design/icons-angular';
+import {
+  AccountBookFill,
+  AlertFill,
+  AlertOutline,
+  UserOutline,
+  TeamOutline,
+  MenuOutline,
+  AndroidOutline,
+  MenuFoldOutline,
+  MenuUnfoldOutline,
+  UsergroupAddOutline,
+  IdcardOutline,
+  GroupOutline,
+  OrderedListOutline,
+  PlusSquareOutline,
+  PieChartOutline,
+  PoweroffOutline,
+} from '@ant-design/icons-angular/icons';
+
+const icons: IconDefinition[] = [
+  AccountBookFill,
+  AlertOutline,
+  AlertFill,
+  UserOutline,
+  TeamOutline,
+  MenuOutline,
+  AndroidOutline,
+  MenuFoldOutline,
+  MenuUnfoldOutline,
+  UsergroupAddOutline,
+  IdcardOutline,
+  GroupOutline,
+  OrderedListOutline,
+  PlusSquareOutline,
+  PieChartOutline,
+  PoweroffOutline,
+];
 
 export const appConfig: ApplicationConfig = {
   providers:
     [
+      provideNzIcons(icons),
       provideZoneChangeDetection({ eventCoalescing: true }),
       provideRouter(routes, withComponentInputBinding()),
-      provideHttpClient(), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(),
+      provideHttpClient(), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(), provideAnimationsAsync(),
     ]
 };
