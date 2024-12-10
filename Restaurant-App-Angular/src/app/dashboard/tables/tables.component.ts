ded6f0bcf8c119e4d0f4b4cff5adec97ba22acab
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-12-10 20:34:10 +0600

    Update tables.component.ts

--- Restaurant-App-Angular/src/app/dashboard/tables/tables.component.ts
+++ Restaurant-App-Angular/src/app/dashboard/tables/tables.component.ts
@@ -1,5 +1,4 @@
 import {Component, effect, inject} from '@angular/core';
-import {AddFoodComponent} from "../foods/add-food/add-food.component";
 import {NzAvatarComponent} from "ng-zorro-antd/avatar";
 import {NzIconDirective} from "ng-zorro-antd/icon";
 import {
@@ -11,38 +10,43 @@ import {
 } from "ng-zorro-antd/table";
 import {FoodBackendService} from '../foods/food-backend.service';
 import {NzModalService} from 'ng-zorro-antd/modal';
+import {TableBackendService} from './table-backend.service';
+import {AvatarToolTipComponent} from './avatar-tool-tip/avatar-tool-tip.component';
 
 @Component({
   selector: 'app-tables',
   standalone: true,
-    imports: [
-        AddFoodComponent,
-        NzAvatarComponent,
-        NzIconDirective,
-        NzTableCellDirective,
-        NzTableComponent,
-        NzTbodyComponent,
-        NzThAddOnComponent,
-        NzThMeasureDirective,
-        NzTheadComponent,
-        NzTrDirective
-    ],
+  imports: [
+    NzAvatarComponent,
+    NzIconDirective,
+    NzTableCellDirective,
+    NzTableComponent,
+    NzTbodyComponent,
+    NzThAddOnComponent,
+    NzThMeasureDirective,
+    NzTheadComponent,
+    NzTrDirective,
+    AvatarToolTipComponent
+  ],
   providers: [NzModalService],
   templateUrl: './tables.component.html',
   styleUrl: './tables.component.css'
 })
 export class TablesComponent {
-  protected backendService = inject(FoodBackendService);
-  listOfFood = this.backendService.listOfFood;
-  imageBaseUrl = 'https://restaurantapi.bssoln.com/images/food/'
+  protected backendService = inject(TableBackendService);
+  listOfTable = this.backendService.listOfTable;
+  listOfEmployees = this.backendService.listOfEmployees;
+  imageBaseUrl = 'https://restaurantapi.bssoln.com/images/table/'
+
 
   ngOnInit() {
-    this.loadDataFromServer(this.pageIndex, this.backendService.totalFood());
+    this.loadDataFromServer(this.pageIndex, this.backendService.totalTable());
   }
 
   pageSize = 10;
   pageIndex = 1;
   constructor() {
+    this.loadDataFromServer(this.pageIndex, this.backendService.totalTable());
     effect(() => {
       if (this.backendService.triggerRefresh()) {
         this.ngOnInit();
@@ -61,12 +65,13 @@ export class TablesComponent {
     pageIndex: number,
     pageSize: number,
   ): void {
-    this.backendService.getListOFFood('', this.pageIndex.toString(), this.pageSize.toString(), '');
+    this.backendService.getListOfTables('', this.pageIndex.toString(), this.pageSize.toString(), '');
+    this.backendService.loadFullListOfEmployee();
   }
 
   deleteFood(id: number) {
     this.backendService.deleteFood(id);
-    if (this.backendService.listOfFood().length <= 1) {
+    if (this.backendService.listOfTable().length <= 1) {
       this.pageIndex = Math.max(1, this.pageIndex - 1);
     }
   }
