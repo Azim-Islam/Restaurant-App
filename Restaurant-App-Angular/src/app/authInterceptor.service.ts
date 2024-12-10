1dba60588259ff74fae0eb0a191ba8e50ac5b235
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-12-10 14:56:22 +0600

    Create authInterceptor.service.ts

---
+++ Restaurant-App-Angular/src/app/authInterceptor.service.ts
@@ -0,0 +1,40 @@
+import {AuthService} from './login/auth.service';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpHandlerFn,
+  HttpInterceptor,
+  HttpRequest
+} from '@angular/common/http';
+import {catchError, Observable, throwError} from 'rxjs';
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.authService.getAuthToken();
+
+    if (token) {
+      request = request.clone({
+        setHeaders: {Authorization: `Bearer ${token}`}
+      });
+    }
+
+    return next.handle(request);
+    //   .pipe(
+    //   catchError((err) => {
+    //     if (err instanceof HttpErrorResponse) {
+    //       if (err.status === 401) {
+    //         // redirect user to the logout page
+    //       }
+    //     }
+    //     return throwError(err);
+    //   })
+    // )
+  }
+}
+
+
+
