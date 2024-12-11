eff59c92c647291b0adcff758acc98fd377a6d83
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-12-11 20:54:40 +0600

    Create add-to-table.component.ts

---
+++ Restaurant-App-Angular/src/app/dashboard/tables/add-to-table/add-to-table.component.ts
@@ -0,0 +1,87 @@
+import {Component, inject} from '@angular/core';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+} from "@angular/forms";
+import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzColDirective, NzRowDirective} from "ng-zorro-antd/grid";
+import {NzDatePickerComponent} from "ng-zorro-antd/date-picker";
+import {NzFormControlComponent, NzFormDirective, NzFormItemComponent, NzFormLabelComponent} from "ng-zorro-antd/form";
+import {NzIconDirective} from "ng-zorro-antd/icon";
+import {NzInputDirective, NzInputGroupComponent} from "ng-zorro-antd/input";
+import {NzModalComponent, NzModalContentDirective, NzModalFooterDirective} from "ng-zorro-antd/modal";
+import {NzOptionComponent, NzSelectComponent} from "ng-zorro-antd/select";
+import {NzUploadComponent} from "ng-zorro-antd/upload";
+import {TableBackendService} from '../table-backend.service';
+import {
+  NzAutocompleteComponent,
+  NzAutocompleteOptionComponent,
+  NzAutocompleteTriggerDirective
+} from 'ng-zorro-antd/auto-complete';
+import {Employee} from '../../employees/employee.interface';
+import {NzAvatarComponent} from 'ng-zorro-antd/avatar';
+
+@Component({
+  selector: 'app-add-to-table',
+  standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    NzButtonComponent,
+    NzColDirective,
+    NzDatePickerComponent,
+    NzFormControlComponent,
+    NzFormDirective,
+    NzFormItemComponent,
+    NzFormLabelComponent,
+    NzIconDirective,
+    NzInputDirective,
+    NzModalComponent,
+    NzOptionComponent,
+    NzRowDirective,
+    NzSelectComponent,
+    NzUploadComponent,
+    NzModalContentDirective,
+    NzModalFooterDirective,
+    NzInputGroupComponent,
+    NzAutocompleteTriggerDirective,
+    NzAutocompleteComponent,
+    NzAutocompleteOptionComponent,
+    NzAvatarComponent,
+  ],
+  templateUrl: './add-to-table.component.html',
+  styleUrl: './add-to-table.component.css'
+})
+export class AddToTableComponent {
+  backendService = inject(TableBackendService);
+  modalWidth = "600px";
+  inputValue?: string;
+  listOfAvailableEmployees : Employee[] = this.backendService.listOfEmployees();
+
+  onChange(e: Event): void {
+    const value = (e.target as HTMLInputElement).value;
+    this.listOfAvailableEmployees = this.backendService.listOfEmployees().filter((emp) => {
+      if ((emp.user.firstName.toLowerCase() +  emp.user.middleName.toLowerCase() + emp.user.lastName.toLowerCase()).includes(value.toLowerCase())) {
+        return emp;
+      }
+      return;
+    })
+  }
+
+
+  submitForm() {
+
+  }
+
+  handleCancel() {
+    this.backendService.showAssignModal.set(false);
+  }
+
+  handleOk() {
+    this.handleCancel();
+  }
+
+  employeeSelected(id: string) {
+    console.log(id);
+  }
+}
