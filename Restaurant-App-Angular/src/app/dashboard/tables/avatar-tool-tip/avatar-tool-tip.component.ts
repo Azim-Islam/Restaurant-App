2a7baa3712fa1135de6a010d174a8d2c8f2d0a40
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-12-11 20:54:45 +0600

    Update avatar-tool-tip.component.ts

--- Restaurant-App-Angular/src/app/dashboard/tables/avatar-tool-tip/avatar-tool-tip.component.ts
+++ Restaurant-App-Angular/src/app/dashboard/tables/avatar-tool-tip/avatar-tool-tip.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, InputSignal} from '@angular/core';
+import {Component, inject, Input, input, InputSignal} from '@angular/core';
 import {NzButtonComponent} from 'ng-zorro-antd/button';
 import {NzTooltipDirective} from 'ng-zorro-antd/tooltip';
 import {Employee, ResponseListOfEmployees} from '../../employees/employee.interface';
@@ -6,6 +6,9 @@ import {NzAvatarComponent} from 'ng-zorro-antd/avatar';
 import {NzModalService} from 'ng-zorro-antd/modal';
 // import {NzTransitionPatchDirective} from 'ng-zorro-antd/core/transition-patch/transition-patch.directive';
 import {NzIconDirective} from 'ng-zorro-antd/icon';
+import {NzContextMenuService, NzDropdownMenuComponent} from 'ng-zorro-antd/dropdown';
+import {NzMenuDirective, NzMenuItemComponent, NzSubMenuComponent} from 'ng-zorro-antd/menu';
+import {TableBackendService} from '../table-backend.service';
 
 
 @Component({
@@ -16,16 +19,25 @@ import {NzIconDirective} from 'ng-zorro-antd/icon';
     NzTooltipDirective,
     NzAvatarComponent,
     NzIconDirective,
+    NzDropdownMenuComponent,
+    NzMenuDirective,
+    NzMenuItemComponent,
+    NzSubMenuComponent,
   ],
   providers: [NzModalService],
   templateUrl: './avatar-tool-tip.component.html',
   styleUrl: './avatar-tool-tip.component.css',
 })
 export class AvatarToolTipComponent {
+  modal = inject(NzModalService);
   employeeId: InputSignal<string> = input.required<string>();
   employeeName: InputSignal<string> = input.required<string>();
   employeeList: InputSignal<Employee[]> = input.required<Employee[]>();
   employeeImageUrl = '';
+  nzContextMenuService = inject(NzContextMenuService);
+  backendService = inject(TableBackendService);
+  employeeTableId = input.required<string>() ;
+  tableName = input.required<string>();
 
   ngOnInit(): void {
     this.employeeList().forEach((employee: Employee) => {
@@ -33,5 +45,23 @@ export class AvatarToolTipComponent {
         this.employeeImageUrl = `https://restaurantapi.bssoln.com/images/user/` + `/` + employee.user.image;
       }
     })
+  }
+
+  contextMenu($event: MouseEvent, menu: NzDropdownMenuComponent): void {
+    this.nzContextMenuService.create($event, menu);
+  }
+
+  removeEmployeeFromTable(){
+    this.backendService.removeEmployeeFromTable(this.employeeTableId());
+  }
+
+  showDeleteConfirm(): void {
+    this.modal.confirm({
+      nzTitle: `Remove "${this.employeeName()}" from ${this.tableName()}?`,
+      nzOkText: 'Confirm Remove',
+      nzOnOk: () => this.removeEmployeeFromTable(),
+      nzCancelText: 'Cancel Operation',
+      nzOnCancel: () => {}
+    });
   }
 }
