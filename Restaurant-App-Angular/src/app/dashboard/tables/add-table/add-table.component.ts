b06410c45ccb18224b8ea7f46364617ef3a81c2a
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-12-12 14:09:41 +0600

    Create add-table.component.ts

---
+++ Restaurant-App-Angular/src/app/dashboard/tables/add-table/add-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-table.component.html',
+  styleUrl: './add-table.component.css'
+})
+export class AddTableComponent {
+
+}
