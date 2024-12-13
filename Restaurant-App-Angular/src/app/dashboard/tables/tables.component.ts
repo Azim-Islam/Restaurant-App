02d35c785569f73dce8651eeab1879409b7d2eb6
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-12-13 20:31:00 +0600

    Update tables.component.ts

--- Restaurant-App-Angular/src/app/dashboard/tables/tables.component.ts
+++ Restaurant-App-Angular/src/app/dashboard/tables/tables.component.ts
@@ -14,6 +14,8 @@ import {AddToTableComponent} from './add-to-table/add-to-table.component';
 import {NzModalService} from 'ng-zorro-antd/modal';
 import {toObservable} from '@angular/core/rxjs-interop';
 import {AddTableComponent} from './add-table/add-table.component';
+import {NzTooltipDirective} from 'ng-zorro-antd/tooltip';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 
 @Component({
@@ -32,6 +34,7 @@ import {AddTableComponent} from './add-table/add-table.component';
     AvatarToolTipComponent,
     AddToTableComponent,
     AddTableComponent,
+    NzTooltipDirective,
   ],
   providers: [NzModalService],
   templateUrl: './tables.component.html',
@@ -45,10 +48,20 @@ export class TablesComponent {
   listOfEmployees = this.backendService.listOfEmployees;
   imageBaseUrl = 'https://restaurantapi.bssoln.com/images/table/'
   private injector = inject(Injector);
-
+  responsive = inject(BreakpointObserver);
+  tableWidthConfig = ['120px', '140px', '110px', '160px', 'auto', '180px'];
 
   ngOnInit() {
     this.loadDataFromServer(this.pageIndex, this.backendService.totalTable());
+
+    this.responsive.observe([Breakpoints.Large, Breakpoints.XLarge])
+      .subscribe(result => {
+        this.tableWidthConfig = ['70px', '100px', '100px', '110px', 'auto', '180px'];
+        if (result.matches) {
+          this.tableWidthConfig = ['120px', '160px', '130px', '180px', 'auto', '180px'];
+        }
+      });
+
   }
 
   pageSize = 10;
